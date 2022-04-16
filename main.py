067f342676586d79e860ad264c087d798525b159
AndemusGBJ <andemus017@outlook.fr>
2022-04-16 13:08:07 +0200

    application des commandes pour déplacer la tortue

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import turtle
 
-t = turtle.Turtle()
+t = turtle.Turtle() # Création de l'objet
 
-turtle.done()
+t.forward(200)  #On avance de 200 pixel
+t.right(90)     #On tourne à droit à 90°
+t.forward(100)  # On avance de 100 pixels
+t.left(90)      # On toure à gauche de 90°
+t.forward(100)  # On avance de 100 pixels
+t.right(90)     # On toure à doite à 90°
+t.backward(300) # On récule de 300 pixels
+
+turtle.done()   #Pour garder la fenêtre ouverte
